29dba49e52b66cd126402c796fb87d24604d796a
Tamanna Akter <96432968+Tasnim63@users.noreply.github.com>
2022-08-31 16:59:54 +0600

    dinamic api added

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-let weather ={
-    "apiKey":"e61b0c1c5c25ffa9d1c2ac461d7ac83d"
-}
+let weather = {
+    apiKey: "e61b0c1c5c25ffa9d1c2ac461d7ac83d",
+  fetchWheather: function (city) {
+  fetch("https://api.openweathermap.org/data/2.5/weather?q="
+   + city +
+    "&units=metric&appid="
+     + this.apiKey).then((response) => response.json()).then((data) => console.log(data))
+  },
+
+  displayWheather: function (data){
+
+  }
+};
